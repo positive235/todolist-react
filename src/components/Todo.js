2c146f024e128eae97a29389a21d4aeb73f53d67
Hae-Ji Park <positive235@gmail.com>
2021-04-30 01:39:53 -0700

    check the completed task or delete the task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const Todo = ({name, checked, id})  => {
+const Todo = ({ addedTasks, setAddedTasks })  => {
+
+  const isCheckedToDo = (e) => {
+    e.preventDefault();
+    addedTasks[e.target.name].checked = !addedTasks[e.target.name].checked;
+    setAddedTasks(prev => [...prev]);
+  };
+
+  const deleteToDo = (e) => {
+      e.preventDefault();
+      addedTasks.splice(e.target.name, 1);
+      console.log(addedTasks);
+      setAddedTasks(prev => [...prev]);
+  }
+
   return (
-    <div>
-      <h3>{id}:{name}</h3>
+    <div className="toDoOuterBlock">
+      {addedTasks.map((addedTask, index) => (
+          <div className="toDoBlock">
+            <button className="toDoButton checkButton" name={index} onClick={isCheckedToDo}> V </button>
+            <h3 className={addedTask.checked ? "isChecked" : "notChecked"}>{addedTask.name}</h3>
+            <button className="toDoButton deleteButton" name={index} onClick={deleteToDo}> X </button>
+          </div>
+      ))}
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,29 +8,22 @@ const TodoList = () => {
   
   const newToDo = (e) => {
     setNewTask(e.target.value);
-  }
+  };
 
   const addToDo = (e) => {
     e.preventDefault();
-    setAddedTasks(prev => [...prev, {name: newTask, checked: false}]);
+    if (newTask !== "") {
+        setAddedTasks(prev => [...prev, {name: newTask, checked: false}]);
+    }
     setNewTask('');
-    
-  }
+  };
 
   return (
     <div>
       <form>
+        <button onClick={addToDo} className="addButton"> + </button>
         <input value={newTask} onChange={newToDo} type="text" className="addToDoBar" placeholder="Add To-Do" />
-        <button onClick={addToDo}>Add</button>
-        
-        {addedTasks.map((addedTask, index) => (
-            <Todo 
-                key={index}
-                name={addedTask.name}
-                checked={addedTask.checked}
-                id={index + 1}
-            />
-        ))}
+        <Todo addedTasks={addedTasks} setAddedTasks={setAddedTasks} />
       </form>
     </div>
   );
